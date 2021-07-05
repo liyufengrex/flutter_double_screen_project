7b144309325deeff22c4aa78b7a41ae69f7c3360
liyufeng <liyufeng@syswin.com>
2021-07-05 11:46:12 +0800

    优化，去掉MainActivity中对副屏初始化的显式调用，在plugin初始化

--- android/app/src/main/kotlin/com/rex/test_double_screen/MainActivity.kt
+++ android/app/src/main/kotlin/com/rex/test_double_screen/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.rex.test_double_screen
-import android.os.Bundle
-import com.rex.flutter_subscreen_plugin.FlutterSubScreenProvider
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //副屏初始化
-        FlutterSubScreenProvider.initSubScreen(context, flutterEngine)
-    }
 }
